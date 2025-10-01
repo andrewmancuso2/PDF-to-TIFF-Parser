d7c1cae890fbc615f656482795afc706f849b629
andrewmancuso2 <andrewmancuso2@gmail.com>
2025-10-01 09:47:08 -0500

    Update PDF2TIFF.py
    - Fixed issue of not uploading next page in range after the initial.

--- PDF2TIFF.py
+++ PDF2TIFF.py
@@ -1,17 +1,13 @@
 # -*- coding: utf-8 -*-
-
+import os
 import arcpy
 
 class Toolbox:
     def __init__(self):
-        """Define the toolbox (the name of the toolbox is the name of the
-        .pyt file)."""
+        """Define the toolbox (the name of the toolbox is the name of the .pyt file)."""
         self.label = "Toolbox"
         self.alias = "toolbox"
-
-        # List of tool classes associated with this toolbox
         self.tools = [Tool]
-
 
 class Tool:
     def __init__(self):
@@ -20,7 +16,6 @@ class Tool:
         self.description = "PDF to TIFF conversion tool"
 
     def getParameterInfo(self):
-
         """Define the tool parameters."""
         params = []
 
@@ -93,43 +88,49 @@ class Tool:
         return True
 
     def updateParameters(self, parameters):
-        """Modify the values and properties of parameters before internal
-        validation is performed.  This method is called whenever a parameter
-        has been changed."""
+        """Called whenever a parameter has been changed."""
         return
 
     def updateMessages(self, parameters):
-        """Modify the messages created by internal validation for each tool
-        parameter. This method is called after internal validation."""
+        """Modify messages created by internal validation."""
         return
 
     def execute(self, parameters, messages):
         """The source code of the tool."""
-        # Get the input PDF file path from the parameters
-        input_pdf_path = parameters[4].valueAsText
-        # Get output folder, page range, project ID, and county
-        output_folder = parameters[5].valueAsText
-        start_page = int(parameters[2].value) - 1  # zero-based index
-        end_page = int(parameters[3].value) - 1
-        project_id = parameters[1].valueAsText
+        # Get inputs in the same positional order as getParameterInfo
         county = parameters[0].valueAsText
+        project_id = parameters[1].valueAsText
+        start_page = int(parameters[2].value) - 1  # zero-based internal
+        end_page = int(parameters[3].value) - 1
+        input_pdf_path = parameters[4].valueAsText
+        output_folder = parameters[5].valueAsText
 
         aprx = arcpy.mp.ArcGISProject("CURRENT")
-        map_obj = aprx.listMaps()[0] 
+        map_obj = aprx.listMaps()[0]
+
+        # Ensure output folder exists
+        if not os.path.isdir(output_folder):
+            os.makedirs(output_folder, exist_ok=True)
 
         for page_num in range(start_page, end_page + 1):
-            pagestring = str(page_num + 1)
-            if page_num <= 10:
-                pagestring = "0" + str(page_num + 1)
+            pagestring = str(page_num + 1).zfill(2)  # 01, 02, 10, ...
             output_filename = f"{project_id}_p{pagestring}_{county}.tif"
-            output_path = arcpy.os.path.join(output_folder, output_filename)
-            arcpy.conversion.PDFToTIFF(input_pdf_path, output_path)
+            output_path = os.path.join(output_folder, output_filename)
+
+            # IMPORTANT: specify the page number (1-based) so each loop exports the correct page
+            # PDFToTIFF(in_pdf_file, out_tiff_file, {pdf_password}, {pdf_page_number})
+            arcpy.conversion.PDFToTIFF(
+                input_pdf_path,
+                output_path,
+                None,            # pdf_password (not used)
+                page_num + 1     # pdf_page_number (1-based)
+            )
+
             map_obj.addDataFromPath(output_path)
 
         del aprx
         return
 
     def postExecute(self, parameters):
-        """This method takes place after outputs are processed and
-        added to the display."""
+        """Runs after outputs are processed and added to the display."""
         return
